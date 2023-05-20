427c0b5d141b1727c8633ea9ba54b06ec1bf6c96
Paulo Moreira <phmoreira37@gmail.com>
2023-05-20 14:38:03 -0300

    feat: refactor memories route get

--- src/routes/meories.ts
+++ src/routes/meories.ts
@@ -4,7 +4,17 @@ import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
-    const users = await prisma.user.findMany()
-    return users
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
   })
 }
