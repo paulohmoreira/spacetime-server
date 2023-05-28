236fe711067f4d27d357880f53c2872fcc1da175
Paulo Moreira <phmoreira37@gmail.com>
2023-05-28 18:03:49 -0300

    feat: upload images and videos

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,19 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
-
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
 app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -19,6 +25,7 @@ app.register(jwt, {
   secret: 'spacetime',
 })
 
+app.register(uploadRoutes)
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
