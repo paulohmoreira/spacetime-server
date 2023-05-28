3dde466838eb1a9df8d8e4455fdec37ffb091cd5
Paulo Moreira <phmoreira37@gmail.com>
2023-05-28 16:28:16 -0300

    feat: install fastfy multipart

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(multipart)
 
 app.register(cors, {
   origin: true,
